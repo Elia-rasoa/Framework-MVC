f28fb0699d3afebc64a79ffb0e86c1b485f99751
Elia-rasoa <eliaonintsoa@gmail.com>
2026-06-10 21:19:25 +0300

    a jour

--- src/main/java/servlet/framework/FrontControllerServlet.java
+++ src/main/java/servlet/framework/FrontControllerServlet.java
@@ -1,3 +1,42 @@
-public class FrontControllerServlet {
-    
+package servlet.framework;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// Cette annotation dit que TOUTES les URLs entrantes passent par ici
+@WebServlet("/") 
+public class FrontControllerServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        // On indique au navigateur qu'on va lui envoyer du texte simple
+        response.setContentType("text/plain;charset=UTF-8");
+        
+        // Étape 2 : On récupère l'URL
+        String urlTapee = request.getRequestURL().toString();
+        
+        // Étape 3 : On l'affiche
+        PrintWriter out = response.getWriter();
+        out.println("URL demandee : " + urlTapee);
+    }
+
+    // 2. Le point d'entrée pour les requêtes de type GET (ex: charger une page)
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    // 3. Le point d'entrée pour les requêtes de type POST (ex: soumettre un formulaire)
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 }
